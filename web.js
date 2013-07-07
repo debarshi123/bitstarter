3bb8133f2fc8384bd1c3d9799cbaae48a93554a1
Debarshi Chakraborty <debarshi123000@yahoo.co.in>
2013-07-07 17:28:25 +0000

    Updated version 6 of web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync("index.html','utf8');
+var buf = fs.readFileSync('index.html','utf8');
 app.get('/', function(request, response) {
   response.send(buf.toString('utf8'));
 });
